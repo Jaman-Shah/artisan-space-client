28d261536906a26c59c532fb3fb48286867ff562
Jaman-Shah <jamanshah5400@gmail.com>
2024-04-27 13:44:53 +0600

    Craft Cards for home page design with dummy image finished

--- src/components/CraftCard/CraftCard.jsx
+++ src/components/CraftCard/CraftCard.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { MdOutlineStarRate } from "react-icons/md";
 
 const CraftCard = ({ craft }) => {
-  const { image, item_name, subcategory_Name, short_description } = craft;
+  const {
+    image,
+    item_name,
+    price,
+    rating,
+    subcategory_Name,
+    short_description,
+  } = craft;
   return (
-    <div>
-      <h1>{item_name}</h1>
+    <div className="p-4 shadow-xl rounded-3xl  ">
+      <div>
+        <img src={image} alt="" className="w-full  rounded-3xl mb-4" />
+      </div>
+      <div className="text-center ">
+        <h3 className="font-bold  text-3xl"> {item_name}</h3>
+        <hr className="my-4 border border-red-600" />
+        <h4 className="my-4 text-2xl text-blue-400">
+          Category:{subcategory_Name}
+        </h4>
+        <p className="my-4 font-bold">{short_description}</p>
+      </div>
+      <div className="flex justify-between px-4">
+        <h4 className="font-bold flex items-center">
+          <p> Price : {price}</p>
+          <RiMoneyDollarCircleLine className="text-yellow-400" />
+        </h4>
+        <h4 className="font-bold flex items-center">
+          <p> Rating : {rating}</p>
+          <MdOutlineStarRate className="text-yellow-400" />
+        </h4>
+      </div>
+      <div className="text-center my-4">
+        <button className="bg-blue-600 p-3 rounded-3xl text-white hover:bg-orange-400">
+          View Details
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/CraftItemsSection/CraftItemsSection.jsx
+++ src/components/CraftItemsSection/CraftItemsSection.jsx
@@ -3,7 +3,8 @@ import CraftCard from "../CraftCard/CraftCard";
 
 const crafts = [
   {
-    image: "pencil_sketch_portrait.jpg",
+    image:
+      "https://img.freepik.com/free-vector/watercolor-oil-painting-background_23-2150133506.jpg",
     item_name: "Pencil Sketch Portrait",
     subcategory_Name: "Portrait Drawing",
     short_description: "Capture lifelike portraits with pencil sketches.",
@@ -13,7 +14,8 @@ const crafts = [
     processing_time: "1-2 weeks",
   },
   {
-    image: "realistic_oil_portrait.jpg",
+    image:
+      "https://img.freepik.com/free-vector/watercolor-oil-painting-background_23-2150133506.jpg",
     item_name: "Realistic Oil Portrait",
     subcategory_Name: "Oil Painting",
     short_description: "Capture realism with oil on canvas.",
@@ -23,7 +25,8 @@ const crafts = [
     processing_time: "2-3 weeks",
   },
   {
-    image: "floral_watercolor_art.jpg",
+    image:
+      "https://img.freepik.com/free-vector/watercolor-oil-painting-background_23-2150133506.jpg",
     item_name: "Floral Watercolor Art",
     subcategory_Name: "Watercolour Painting",
     short_description: "Vibrant watercolor artwork featuring flowers.",
@@ -33,7 +36,8 @@ const crafts = [
     processing_time: "1-2 weeks",
   },
   {
-    image: "figure_charcoal_sketch.jpg",
+    image:
+      "https://img.freepik.com/free-vector/watercolor-oil-painting-background_23-2150133506.jpg",
     item_name: "Figure Charcoal Sketch",
     subcategory_Name: "Charcoal Sketching",
     short_description: "Expressive sketches using charcoal.",
@@ -46,11 +50,29 @@ const crafts = [
 
 const CraftItemsSection = () => {
   return (
-    <div className="relative -top-100">
-      {crafts &&
-        crafts.map((craft) => {
-          return <CraftCard key={craft.image} craft={craft} />;
-        })}
+    <div>
+      <div className="p-4 md:p-20">
+        <div className="divider divider-primary">
+          <h3 className="text-2xl md:text-4xl font-extrabold">
+            Let's <span className="text-blue-500">See</span> Our{" "}
+            <span className="text-orange-400">Items</span>
+          </h3>
+        </div>
+        <p className="text-center font-bold">
+          We are determined to provide you the attracting arts and crafts with
+          cheap cost. But don't worry they are unique and looking good. We are
+          determined to provide you the attracting arts and crafts with cheap
+          cost. But don't worry they are unique and looking good. We are
+          determined to provide you the attracting arts and crafts with cheap
+          cost. But don't worry they are unique and looking good
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {crafts &&
+          crafts.map((craft) => {
+            return <CraftCard key={craft.image} craft={craft} />;
+          })}
+      </div>
     </div>
   );
 };
