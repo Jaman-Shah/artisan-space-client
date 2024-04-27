aa022b09ee23d4acbafdddc438ae2f014339bd88
Jaman-Shah <jamanshah5400@gmail.com>
2024-04-27 20:35:06 +0600

    my art and craft page card design and showing information finished

---
+++ src/components/IndividualCraftCard/IndividualCraftCard.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { MdOutlineStarRate } from "react-icons/md";
+
+const IndividualCraftCard = ({ craft }) => {
+  const {
+    customization,
+    image,
+    item_name,
+    price,
+    rating,
+    short_description,
+    stock_status,
+    subcategory_name,
+    user_email,
+  } = craft;
+  return (
+    <div className="p-4 shadow-xl rounded-3xl  ">
+      <div>
+        <img src={image} alt="" className="w-full  rounded-3xl mb-4" />
+      </div>
+      <div className="text-center ">
+        <h3 className="font-bold  text-3xl"> {item_name}</h3>
+        <hr className="my-4 border border-red-600" />
+        <h4 className="my-4 text-2xl text-blue-400">
+          Category:{subcategory_name}
+        </h4>
+        <p className="my-4 font-bold">{short_description}</p>
+      </div>
+      <div className="flex justify-between px-4">
+        <h4 className="font-bold flex items-center">
+          <p> Price : {price}</p>
+          <RiMoneyDollarCircleLine className="text-yellow-400" />
+        </h4>
+        <h4 className="font-bold flex items-center">
+          <p> Rating : {rating}</p>
+          <MdOutlineStarRate className="text-yellow-400" />
+        </h4>
+      </div>
+      <div className="flex justify-between mt-2">
+        <p>Customizable : {customization}</p>
+        <p>Stoke Status : {stock_status}</p>
+      </div>
+      <div className="flex justify-center gap-5 my-4">
+        <button className="bg-green-600 p-3 rounded-3xl text-white hover:bg-orange-400">
+          Update
+        </button>
+        <button className="bg-red-600 p-3 rounded-3xl text-white hover:bg-orange-400">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default IndividualCraftCard;
--- src/components/MyArtAndCraft/MyArtAndCraft.jsx
+++ src/components/MyArtAndCraft/MyArtAndCraft.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import IndividualCraftCard from "../IndividualCraftCard/IndividualCraftCard";
 
 const MyArtAndCraft = () => {
-  return <div>this is my art and craft</div>;
+  const { user } = useContext(AuthContext);
+  console.log(user.email);
+  const [loadedCrafts, setLoadedCrafts] = useState();
+  console.log(loadedCrafts);
+
+  // loading the crafts based on email
+  const loadCrafts = async () => {
+    fetch(`http://localhost:5003/getcraftsbyemail/${user.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setLoadedCrafts(data);
+      });
+  };
+
+  // calling the loadCrafts function inside useEffect
+  useEffect(() => {
+    loadCrafts();
+  }, []);
+  return (
+    <div>
+      <div className="my-8">
+        <h1 className="font-extrabold text-4xl text-center">
+          Your<span className="text-orange-500"> Art</span> And{" "}
+          <span className="text-blue-500">Crafts</span> List
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {loadedCrafts &&
+          loadedCrafts.map((craft) => {
+            return <IndividualCraftCard key={craft._id} craft={craft} />;
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default MyArtAndCraft;
