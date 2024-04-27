c8af87a1fe263b964b992e883e80553cc66075bd
Jaman-Shah <jamanshah5400@gmail.com>
2024-04-27 14:11:45 +0600

    footer section added finished

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Slider from "../Slider/Slider";
 import CraftItemsSection from "../CraftItemsSection/CraftItemsSection";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Slider />
       <CraftItemsSection />
+      <Footer />
     </div>
   );
 };
