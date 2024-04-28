9c78353855d48f0e5df2d86b7e19e987d8e23850
Jaman-Shah <jamanshah5400@gmail.com>
2024-04-29 03:00:11 +0600

    category based page dynamic title finished

--- src/components/SubCategoryPage/SubCategoryPage.jsx
+++ src/components/SubCategoryPage/SubCategoryPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import SubCategoryPageCard from "../SubCategoryPageCard/SubCategoryPageCard";
 
 const SubCategoryPage = () => {
+  const { subcategory } = useParams();
   const loadedCraft = useLoaderData();
 
   console.log(loadedCraft);
   return (
     <div>
-      <div></div>
+      <div className="my-4">
+        <h1 className="font-bold text-4xl text-center">
+          <span className="text-blue-500"> {subcategory} </span>
+          <span className="text-orange-500">Items :</span>
+        </h1>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {loadedCraft &&
           loadedCraft.map((craft) => {
