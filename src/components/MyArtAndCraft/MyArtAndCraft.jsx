24466cee33b4d4cee3129df91607393f5475db9f
Jaman-Shah <jamanshah5400@gmail.com>
2024-04-28 18:55:06 +0600

    filter in my art and craft section finished

--- src/components/MyArtAndCraft/MyArtAndCraft.jsx
+++ src/components/MyArtAndCraft/MyArtAndCraft.jsx
@@ -6,6 +6,8 @@ const MyArtAndCraft = () => {
   const { user } = useContext(AuthContext);
 
   const [loadedCrafts, setLoadedCrafts] = useState([]);
+  const [filteredCrafts, setFilteredCrafts] = useState([]);
+  const [customization, setCustomization] = useState("");
 
   // loading the crafts based on email
   const loadCrafts = async () => {
@@ -13,6 +15,7 @@ const MyArtAndCraft = () => {
       .then((res) => res.json())
       .then((data) => {
         setLoadedCrafts(data);
+        setFilteredCrafts(data);
       });
   };
 
@@ -20,6 +23,19 @@ const MyArtAndCraft = () => {
   useEffect(() => {
     loadCrafts();
   }, []);
+
+  const handleCustomizationChange = (event) => {
+    const selectedCustomization = event.target.value;
+    if (selectedCustomization === "Customization") {
+      setFilteredCrafts(loadCrafts);
+    }
+    setCustomization(selectedCustomization);
+    const filteredData = loadedCrafts.filter((element) => {
+      return element.customization == selectedCustomization;
+    });
+    setFilteredCrafts(filteredData);
+  };
+
   return (
     <div>
       <div className="my-8">
@@ -28,18 +44,32 @@ const MyArtAndCraft = () => {
           <span className="text-blue-500">Crafts</span> List
         </h1>
       </div>
+      <div>
+        <h1 className="text-center my-4 text-3xl font-bold ">
+          Filter By <span className="text-orange-500">Customization :</span>
+        </h1>
+        <div className="text-center h-16">
+          <select
+            name="customization"
+            value={customization}
+            onChange={handleCustomizationChange}
+            className="h-full p-4 bg-blue-300"
+          >
+            <option value="Customization">Customization</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+        </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {loadedCrafts &&
-          loadedCrafts.map((craft) => {
-            return (
-              <IndividualCraftCard
-                key={craft._id}
-                craft={craft}
-                loadedCrafts={loadedCrafts}
-                setLoadedCrafts={setLoadedCrafts}
-              />
-            );
-          })}
+        {filteredCrafts.map((craft) => (
+          <IndividualCraftCard
+            key={craft._id}
+            craft={craft}
+            loadedCrafts={loadedCrafts}
+            setLoadedCrafts={setLoadedCrafts}
+          />
+        ))}
       </div>
     </div>
   );
