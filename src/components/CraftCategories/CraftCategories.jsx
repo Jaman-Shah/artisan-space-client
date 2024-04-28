560560d305191b11be3e57e462370d7024e46993
Jaman-Shah <jamanshah5400@gmail.com>
2024-04-29 01:04:23 +0600

    home page responsive copletely finished

--- src/components/CraftCategories/CraftCategories.jsx
+++ src/components/CraftCategories/CraftCategories.jsx
@@ -29,7 +29,7 @@ const CraftCategories = () => {
         </div>
         <p></p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {categories &&
           categories.map((category) => {
             return <CategoryCard key={categories._id} category={category} />;
